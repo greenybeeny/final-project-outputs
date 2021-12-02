39968b3f28fc39328b8eddc1fad03c1ddb6e7e72
greenybeeny <92817241+greenybeeny@users.noreply.github.com>
2021-12-02 14:14:38 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        for (let index = 0; index < 4; index++) {
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            basic.pause(500)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            basic.pause(500)
+        }
+        for (let index = 0; index < 4; index++) {
+            basic.showIcon(IconNames.Square)
+            basic.pause(500)
+            basic.showIcon(IconNames.SmallSquare)
+            basic.pause(500)
+        }
+        for (let index = 0; index < 4; index++) {
+            music.setVolume(255)
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            basic.pause(500)
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            basic.pause(500)
+            music.playMelody("D - D - D - D - ", 60)
+        }
+    } else if (receivedNumber == 0) {
+        basic.showString("No Movement")
+        basic.showIcon(IconNames.Yes)
+    }
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        music.setVolume(255)
+        music.playTone(698, music.beat(BeatFraction.Double))
+    }
 })
+radio.setGroup(200)
