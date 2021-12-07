bae61e14e07dd4f192814b8a4fa502ba6432671d
greenybeeny <92817241+greenybeeny@users.noreply.github.com>
2021-12-07 14:09:00 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,33 +3,50 @@ radio.onReceivedNumber(function (receivedNumber) {
     // if the microbit receives the number 1
     // if the microbit receives the number 0 then it tells that there is no movement
     if (receivedNumber == 1) {
-        // make the LED flash 4 times
-        for (let index = 0; index < 4; index++) {
+        // make the buzzer go off 4 times
+        for (let index = 0; index < 1; index++) {
+            music.setVolume(255)
+            music.playMelody("C5 C C5 C C5 C C5 C ", 90)
             pins.digitalWritePin(DigitalPin.P1, 1)
             basic.pause(500)
             pins.digitalWritePin(DigitalPin.P1, 0)
             basic.pause(500)
-        }
-        // this makes the LED on the microbit flash in and out like a visible alarm
-        for (let index = 0; index < 4; index++) {
-            basic.showIcon(IconNames.Square)
-            basic.pause(500)
-            basic.showIcon(IconNames.SmallSquare)
-            basic.pause(500)
-        }
-        // make the buzzer go off 4 times
-        for (let index = 0; index < 4; index++) {
-            music.setVolume(255)
-            pins.digitalWritePin(DigitalPin.P0, 1)
-            basic.pause(500)
-            pins.digitalWritePin(DigitalPin.P0, 0)
-            basic.pause(500)
-            music.playMelody("C5 C C5 C C5 C C5 C ", 60)
+            for (let index = 0; index < 25; index++) {
+                images.createBigImage(`
+                    # # # # # . . . . .
+                    # . . . # . # # # .
+                    # . . . # . # . # .
+                    # . . . # . # # # .
+                    # # # # # . . . . .
+                    `).scrollImage(1, 7.5)
+            }
         }
     } else if (receivedNumber == 0) {
-        basic.showString("No Movement")
-        basic.showIcon(IconNames.Yes)
+        basic.pause(500)
+        for (let index = 0; index < 1; index++) {
+            images.createBigImage(`
+                # . . . # . # # # .
+                # # . . # . # . # .
+                # . # . # . # . # .
+                # . . # # . # . # .
+                # . . . # . # # # .
+                `).scrollImage(1, 100)
+        }
+        basic.pause(500)
+    } else if (receivedNumber == 2) {
+        for (let index = 0; index < 1; index++) {
+            images.createBigImage(`
+                . . . . . . . . . .
+                . . . . . . . . . .
+                . . . . . . . . . .
+                . . . . . . . . . .
+                . . . . . . . . . .
+                `).scrollImage(1, 500)
+        }
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
     }
 })
 // our radio group is 200
 radio.setGroup(200)
+let receivedNumber = 2
